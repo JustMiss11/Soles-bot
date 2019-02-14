7d1a0fefed943598e1d85bc10b75cb79ff37fa02
JustMiss11 <47405121+JustMiss11@users.noreply.github.com>
2019-02-14 19:27:11 +0100

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,19 @@
+const Botconfig = require("../botconfig.json");
+const Discord = require("discord.js");
+//const ms = require("ms")
+
+module.exports.run = async (bot, message, args) => {
+   const embed = new Discord.RichEmbed()
+   .setAuthor("Help commands!")
+   .setColor("GREEN")
+   .setFooter("v-0.0.1-Alpha! Im back!")
+   .setTimestamp()
+   .setDescritpion("NSFW :smirk: \n```]feet, ]cfeet, ]anime-feet, \n ]gif, ]anal``` \nFUN :smile: \n```]8ball, ]coinflip, ]roll, \n ]mass-roll```);
+   
+   message.channel.send(embed)
+}
+
+module.exports.help = {
+    name: "help",
+    aliases: []
+}
