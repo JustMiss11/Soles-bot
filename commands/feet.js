f2b5b6fbbc5dc124667216a52bb56eb18a735a6b
JustMiss11 <47405121+JustMiss11@users.noreply.github.com>
2019-02-14 21:47:40 +0100

    Update feet.js

--- commands/feet.js
+++ commands/feet.js
@@ -1,28 +1,32 @@
-const Discord = require("discord.js");
-const randompuppy = require('random-puppy');
-
+const randomPuppy = require('random-puppy'); 
 
 module.exports.run = async (bot, message, args) => {
- 
 
-    let api = ["Feet",
-               "feet",
-               "female-feet",
-               "male-feet"
-    ]
-      randompuppy(api).then(api => {
-           const theirembed = new Discord.RichEmbed()
-	          .setAuthor("This is nice feets :smirk:")
-            .setColor(0xff9000)
-            .setImage(api)
-            .setFooter("God Bot")
-            .setTimestamp();
-      message.channel.send(theirembed)
-      })
-     }
-   
+ let reddit = [
+                      
+                       
+                       
+                       "male-feet",
+                       "female-feet", 
+                       "feet"
+  ]
+
+let subreddit = reddit[Math.floor(Math.random() * reddit.length)];
+
+ message.channel.startTyping(); 
+
+randomPuppy(subreddit).then(async url => {
+                    await message.channel.send({
+                            files: [{ 
+                                  attachment: url, 
+                                  name: 'meme.png' 
+                          }]           
+                   }).then(() => message.channel.stopTyping()); 
+    }).catch(err => console.error(err)); 
+
+};
 
  module.exports.help = {
-           name: 'feet',
-	   aliases: ['f']
-}    
+           name: 'meme', 
+           aliases: ['memes']
+} 
