93b23d3c821a8df577150582ef1e0a55bab4733b
JustMiss11 <47405121+JustMiss11@users.noreply.github.com>
2019-02-14 21:08:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ bot.on("message", async message => {
  // const url = args[1];
   //const serverQueue = queue.get(message.guild.id);
   
-  let command = args.shift().toLowerCastoLowerCa
+  let command = args.shift();
   let commandFile = require(`./commands/${command}.js`);
   commandFile.run(bot, message, args);
 
