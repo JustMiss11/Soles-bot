f16410ac8c455489f23390d699bbeacd660f0d42
JustMiss11 <47405121+JustMiss11@users.noreply.github.com>
2019-02-14 20:12:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -26,9 +26,9 @@ fs.readdir("./commands/", (err, files) => {
   }
 
   jsfile.forEach((f, i) =>{
-    let props = require(`./commands/${f}`);
+    let module = require(`./commands/${f}`);
     console.log(`${f} loaded!`);
-    bot.commands.set(props.help.name, props);
+    bot.commands.set(module.help.name, module);
   });
 //wrf
 });
