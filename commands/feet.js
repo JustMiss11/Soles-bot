f08804eee9a413bc39b9be64089df3f95cd7a859
JustMiss11 <47405121+JustMiss11@users.noreply.github.com>
2019-02-14 21:40:53 +0100

    Create feet.js

---
+++ commands/feet.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+const randompuppy = require('random-puppy');
+
+
+module.exports.run = async (bot, message, args) => {
+ 
+
+    let api = ["Feet",
+               "feet",
+               "female-feet",
+               "male-feet"
+    ]
+      randompuppy(api).then(api => {
+           const theirembed = new Discord.RichEmbed()
+	          .setAuthor("This is nice feets :smirk:")
+            .setColor(0xff9000)
+            .setImage(api)
+            .setFooter("God Bot")
+            .setTimestamp();
+      message.channel.send(theirembed)
+      })
+     }
+   
+
+ module.exports.help = {
+           name: 'feet'	
+}    
