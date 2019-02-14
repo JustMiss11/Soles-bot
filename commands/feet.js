9aa2fb9f9e7a64405b4c8ddcc94a042025ca465b
JustMiss11 <47405121+JustMiss11@users.noreply.github.com>
2019-02-14 21:59:11 +0100

    Update feet.js

--- commands/feet.js
+++ commands/feet.js
@@ -1,33 +1,23 @@
-const randomPuppy = require('random-puppy'); 
+const discord = require('discord.js');
+const superagent = require('superagent')
 
-module.exports.run = async (bot, message, args) => {
-
- let reddit = [
-                      
-                 
-             
-                       
-                       "male-feet",
-                       "female-feet", 
-                       "feet"
-  ]
-
-let subreddit = reddit[Math.floor(Math.random() * reddit.length)];
-
-  
-
-randomPuppy(subreddit).then(async url => {
-                    await message.channel.send({
-                            files: [{ 
-                                  attachment: url, 
-                                  name: 'Some nice feets.png' 
-                          }]           
-                   })
-    }).catch(err => console.error(err)); 
-
+exports.run = (client, msg, args) => {
+  if (msg.channel.nsfw === true) {
+    superagent.get('https://nekobot.xyz/api/image')
+    .query({ type: 'feet'})
+    .end((err, response) => {
+      var embed = new Discord.RichEmbed()
+      .setAuthor("Some nice feets here ;)")
+      .setColor("RED")
+      .setImage(response.body.message);
+     msg.channel.send(embed)
+    //  msg.channel.send({ file: response.body.message });
+    });
+  } else {
+    msg.channel.send("This isn't NSFW channel!")
+  }
 };
-
- module.exports.help = {
-           name: 'feet', 
-           aliases: ['feets']
+module.exports.help = {
+           name: 'pgif', 
+           aliases: ['tits']
 } 
